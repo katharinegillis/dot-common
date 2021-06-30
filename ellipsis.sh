b54448f40b6d94a7c78d7469bd684b0fd95a6c4d
Katharine Cordes <gillis.katharine@gmail.com>
2021-06-30 15:18:52 -0600

    fix: added package summary tracking

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-# Empty as this installs nothing.
+source "$PKG_PATH/../common/common.sh"
+
+pkg.install() {
+    echo "$PKG_PATH" >> "$HOME/ellipsis_installed.log"
+}
+
+pkg.pull() {
+    # Check for updates on git
+    git remote update 2>&1 > /dev/null
+    if git.is_behind; then
+        # Pull down the updates
+        git.pull
+
+        echo "$PKG_PATH" >> "$HOME/ellipsis_updated.log"
+    else
+        echo "No updates to download."
+        echo "$PKG_PATH" >> "$HOME/ellipsis_unchanged.log"
+    fi
+}
+
+pkg.uninstall() {
+    echo "$PKG_PATH" >> "$HOME/ellipsis_uninstalled.log"
+}
